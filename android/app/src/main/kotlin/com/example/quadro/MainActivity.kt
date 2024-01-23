43642700fd2b1fbe8403c1cdf452dbb84755ebaf
ZahreddenZromly <zahredden1212@gmail.com>
2024-01-23 14:44:33 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/quadro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quadro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+void main (){
+  runApp(app)
+  
+}
